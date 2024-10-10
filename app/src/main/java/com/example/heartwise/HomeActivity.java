23226ac25bf62c4ce01e9de5f24030c0b8785710
httpblad <bulanonbladymyr@gmail.com>
2024-10-10 13:26:33 -0700

    update-for-homeactivity

--- app/src/main/java/com/example/heartwise/HomeActivity.java
+++ app/src/main/java/com/example/heartwise/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.heartwise;
 
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -13,8 +15,16 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_home);
 
+        Button startButton = findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Use HomeActivity.this instead of MainActivity.this
+                Intent intent = new Intent(HomeActivity.this, GetStarted.class);
+                startActivity(intent);
+            }
+        });
     }
 }
